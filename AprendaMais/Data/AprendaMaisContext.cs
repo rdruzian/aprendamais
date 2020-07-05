ea9174b2fa757b412dfb741dd61baeda7073fefd
Renato Druzian <rendruzian@gmail.com>
2020-07-05 00:18:36 -0300

    Correção na criação do banco via EF Core

--- AprendaMais/Data/AprendaMaisContext.cs
+++ AprendaMais/Data/AprendaMaisContext.cs
@@ -1,25 +1,26 @@
 ﻿using AprendaMais.Model;
 using AprendaMais.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AprendaMais.Data
 {
     public class AprendaMaisContext : DbContext
     {
         //Tabelas
-        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Aluno> Aluno { get; set; }
         public DbSet<AlternativaPergunta> AlternativaPergunta { get; set; }
-        public DbSet<Avaliacao> Avaliacoes { get; set; }
-        public DbSet<Disciplina> Disciplinas { get; set; }
-        public DbSet<Pergunta> Perguntas { get; set; }
-        public DbSet<Texto> Textos { get; set; }
-        public DbSet<Universidade> Universidades { get; set; }
+        public DbSet<Avaliacao> Avaliacao { get; set; }
+        public DbSet<Disciplina> Disciplina { get; set; }
+        public DbSet<Pergunta> Pergunta { get; set; }
+        public DbSet<Texto> Texto { get; set; }
+        public DbSet<Universidade> Universidade { get; set; }
+        public DbSet<Dissertativa> Dissertativa { get; set; }
+        public DbSet<AlternativaPergunta> AlternativaPerguntas { get; set; }
 
         //Tabelas de ligações
+        public DbSet<AvaliacaoAluno> AvaliacaoAluno { get; set; }
+        public DbSet<AvaliacaoPergunta> AvaliacaoPergunta { get; set; }
+        public DbSet<PerguntaDisciplina> PerguntaDisciplina { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
