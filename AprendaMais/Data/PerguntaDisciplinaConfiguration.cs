5a6c8bd8723dbd7670ee7a9b346e3168d3cdf7eb
Renato Druzian <rendruzian@gmail.com>
2020-07-04 22:38:21 -0300

    Correções de tipos

--- AprendaMais/Data/PerguntaDisciplinaConfiguration.cs
+++ AprendaMais/Data/PerguntaDisciplinaConfiguration.cs
@@ -20,7 +20,7 @@ namespace AprendaMais.Data
             builder.HasKey("perguntaID", "disciplinaID");
 
             builder.HasOne(pd => pd.Pergunta).WithMany(p => p.Disciplinas).HasForeignKey("perguntaID");
-            builder.HasOne(pd => pd.Disciplina).WithMany(d => d.Perguntas).HAsForeignKey("disciplinaID");
+            builder.HasOne(pd => pd.Disciplina).WithMany(d => d.Perguntas).HasForeignKey("disciplinaID");
         }
     }
 }
